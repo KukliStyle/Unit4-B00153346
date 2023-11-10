799aebc2ab882c6bbc227c58a9bd6b052c80380b
KukliStyle <B00153346@mytudublin.ie>
2023-11-10 01:00:22 +0000

    final

---
+++ unit 4 B00153346/Assets/Challenge 4/Scripts/OpenLink.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink : MonoBehaviour
+{
+    public void Openurl(){
+
+    Application.OpenURL("https://github.com/KukliStyle/Unit4-B00153346");
+    }
+
+}
